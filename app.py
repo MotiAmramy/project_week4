4bc4e7726f7243689e007d6fafc4685a1fba0145
moti <mordechayamramy@gmail.com>
2024-09-26 17:16:22 +0300

    added mission

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+
+from controllers.mission_controller import mission_blueprint
 from controllers.targets_controller import target_blueprint
 
 app = Flask(__name__)
@@ -6,4 +8,5 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
     app.register_blueprint(target_blueprint, url_prefix='/api/targets')
+    app.register_blueprint(mission_blueprint, url_prefix='/api/missions')
     app.run(debug=True)
--- models/__init__.py
+++ models/__init__.py
@@ -2,3 +2,4 @@ from .Cities import cities
 from .Targets import targets
 from .TargetTrpes import targettypes
 from .Countries import countries
+from .mission import Mission
